0c9ec5abd055cc4364be9da52253c633d3a552ae
rasheed.clouddb@gmail.com <rasheed.clouddb@gmail.com>
2024-11-28 04:01:19 +0000

    seedupdate

--- models/abc.sql
+++ models/abc.sql
@@ -1,8 +1,9 @@
 {{
     config(
         materialized='view',
-        secure=true,
+        secure= false,
+        alias='data_analyst',
         query_tag='dbt12'
         )
 }}
-select 1 as id ,'Rasheed' AS  name ,'data_analyst' job
+select 1 as id ,'Rasheed' AS  name ,'data_analyst' job,1500000 salary
--- models/test.sql
+++ models/test.sql
@@ -1,7 +1,9 @@
 {{
     config(
         materialized='view',
-        secure= true
+        secure= true,
+        alias='abcd123',
+        query_tag='dbt123'
     )
 }}
 select * ,dense_rank() over (order by C_ACCTBAL desc ) as ACCOUNT_Bal
